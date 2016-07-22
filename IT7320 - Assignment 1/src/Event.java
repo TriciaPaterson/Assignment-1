b0c5907878bddf2df5e6c52e45944cc27c4d6182
patricia.paterson <patricia.paterson@B106-04.soit.local>
2016-07-22 12:09:06 +1200

    Created interface EventList and methods. Created Event class and a main within Event class

---
+++ IT7320 - Assignment 1/src/Event.java
@@ -0,0 +1,54 @@
+
+public class Event implements EventList {
+
+	private static String eventName;
+	private static String eventHeadline;
+	
+	public Event()
+	{
+		
+	}
+	
+	public Event(String n, String d)
+	{
+		eventName = n;
+		eventHeadline = d;
+	}
+	
+	@Override
+	public String setEventName(String eName) {
+		
+		return eventName = eName;
+	}
+	
+	@Override
+	public String setHeadline(String eHeadline) {
+		
+		return eventHeadline = eHeadline;		
+	}
+
+	@Override
+	public void display(String eventName, String eventHeadline) {
+	
+		System.out.println(eventName + "\t" + eventHeadline);		
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("Event\t\tEvent Headline\n" + 
+				"-----------------------------------------------------------------------------\n");
+		Event e = new Event();
+		e.setEventName("Burgulary");  //Name of event
+		e.setHeadline("Alarm going male seen running from the scene"); // initial information from caller
+		e.display(eventName, eventHeadline);
+		e.setEventName(" \t");
+		e.setHeadline("TD 2 minutes"); // TD means Time Delay. The rest of the headlines are updated information coming in from call taker.
+		e.display(eventName, eventHeadline);
+		e.setHeadline("DOT right on to Skyline Street"); // DOT means Direction of travel. Updated information
+		e.display(eventName, eventHeadline);
+		e.setHeadline("MOT on foot"); // MOT means Mode of travel. Updated information
+		e.display(eventName, eventHeadline);
+		e.setHeadline("Male caucasian green hoodie dark coloured pants");// Updated information
+		e.display(eventName, eventHeadline);	
+	}
+
+}
---
+++ IT7320 - Assignment 1/src/EventList.java
@@ -0,0 +1,9 @@
+
+public interface EventList {
+
+	String setEventName(String n);
+	String setHeadline(String d);
+	void display(String n, String d);	
+	
+}
+
